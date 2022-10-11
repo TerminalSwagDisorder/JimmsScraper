5f3f23c6c8bafbb80491097fbada9f583180381f
TerminalSwagDisorder <benniw139@gmail.com>
2022-10-11 10:25:01 +0300

    Scrape the next page URL

--- Scraper.py
+++ Scraper.py
@@ -11,17 +11,32 @@ component_URL2 = ["/fi/Product/List/000-00K/komponentit--kiintolevyt-ssd-levyt",
 for item in component_URL:
     page = requests.get(URL + item, allow_redirects=True)
     soup = BeautifulSoup(page.content, "html.parser")
+
+    #Get next page url
+    listz = []
+    next_Page = soup.find("div", class_="listpager").find("li", class_="next").find_all("a", href=True)
+    for z in next_Page:
+        listz.append(z["href"])
+    print("listz", listz)
+    for z in listz:
+        next_URL = URL + item + z
+        print(next_URL)
+        
+    #Get the links to all the components on the current page
     try:
         results = soup.find("div", class_="p_name").find_all("a")
     except:
         pass
+    print("results:", results)
     listx = []
-    for x in soup.find_all('a', href=True):
+    for x in soup.find_all("a", href=True):
         listx.append(x["href"]) 
         listy = [x for x in listx if x.startswith(base_URL)]
         listy = list(dict.fromkeys(listy))
-    print(listy)
-    
+    #print(listy)
+    print("curr page", page.url)
+        
+    #Get the text in the item pages
     for y in listy:
         item_Page = requests.get(URL + y, allow_redirects=True)
         Item_soup = BeautifulSoup(item_Page.content, "html.parser")
@@ -35,7 +50,8 @@ for item in component_URL:
                 results_Item = stripchars[1]
             except:
                 pass
-        print(results_Item)
+        #print(results_Item)
+        
         
 
 
