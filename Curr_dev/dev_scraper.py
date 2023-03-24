1c98b61178f9a08ef962bc44fa3becae2aea99ab
TerminalSwagDisorder <benniw139@gmail.com>
2023-03-24 14:28:04 +0200

    New file for collaborative development

---
+++ Curr_dev/dev_scraper.py
@@ -0,0 +1,16 @@
+# File name: dev_scraper.py
+# Auth: Benjamin Willför/TerminalSwagDisorder & Sami Wazni
+# Desc: File currently in development containing code for a scraper for jimms.com
+
+def main():
+	pass
+	
+	
+#Database part
+
+
+
+#Scraper part
+
+
+main()
