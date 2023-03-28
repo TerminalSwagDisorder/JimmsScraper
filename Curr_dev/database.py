ea372a74aad5673e463de788a3a5dc7b9bf2db78
TerminalSwagDisorder <benniw139@gmail.com>
2023-03-28 14:48:07 +0300

    Started to convert all tables to classes

--- Curr_dev/database.py
+++ Curr_dev/database.py
@@ -9,6 +9,7 @@ from sqlalchemy import *
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql.expression import ColumnClause
 from sqlalchemy.sql import table, column, select, update, insert, delete, text
+from sqlalchemy.ext.declarative import declarative_base
 
 fPath = os.path.abspath(os.path.realpath(__file__))
 dPath = os.path.dirname(fPath)
@@ -28,172 +29,24 @@ metadata = MetaData()
 main_parts = ["cpu", "gpu", "cooler", "motherboard", "memory", "storage", "psu", "case"]
 
 # Create table in database	   
-cpu = Table("cpu", metadata,
-	Column("id", INTEGER, primary_key=True, autoincrement=True),
-	Column("Name", TEXT),
-	Column("Manufacturer", TEXT),
-	Column("Core Count", TEXT),
-	Column("Performance Core Clock", TEXT),
-	Column("Performance Boost Clock", TEXT),
-	Column("TDP", TEXT),
-	Column("Integrated Graphics", TEXT),
-	Column("L3 Cache", TEXT),
-	Column("Simultaneous Multithreading", TEXT),
-	Column("Includes CPU Cooler", TEXT),
-	Column("Socket", TEXT),
-	Column("Price", TEXT),
-	Column("Url", TEXT)
-)
-gpu = Table("gpu", metadata,
-	Column("id", INTEGER, primary_key=True, autoincrement=True),
-	Column("Name", TEXT),
-	Column("Manufacturer", TEXT),
-	Column("Chipset", TEXT),
-	Column("Memory", TEXT),
-	Column("Memory Type", TEXT),
-	Column("Core Clock", TEXT),
-	Column("Boost Clock", TEXT),
-	Column("Effective Memory Clock", TEXT),
-	Column("Interface", TEXT),
-	Column("Color", TEXT),
-	Column("Length", TEXT),
-	Column("TDP", TEXT),
-	Column("Expansion Slot Width", TEXT),
-	Column("Cooling", TEXT),
-	Column("Price", TEXT),
-	Column("Url", TEXT)
-)
-cooler = Table("cpu cooler", metadata,
-	Column("id", INTEGER, primary_key=True, autoincrement=True),
-	Column("Name", TEXT),
-	Column("Manufacturer", TEXT),
-	Column("Fan RPM", TEXT),
-	Column("Noise Level", TEXT),
-	Column("Color", TEXT),
-	Column("Height", TEXT),
-	Column("Water Cooled", TEXT),
-	Column("Fanless", TEXT),
-	Column("Price", TEXT),
-	Column("Url", TEXT)
-)
-motherboard = Table("motherboard", metadata,
-	Column("id", INTEGER, primary_key=True, autoincrement=True),
-	Column("Name", TEXT),
-	Column("Manufacturer", TEXT),
-	Column("Socket / CPU", TEXT),
-	Column("Form Factor", TEXT),
-	Column("Chipset", TEXT),
-	Column("Memory Max", TEXT),
-	Column("Memory Type", TEXT),
-	Column("Memory Slots", TEXT),
-	Column("Color", TEXT),
-	Column("PCIe x16 Slots", TEXT),
-	Column("PCIe x8 Slots", TEXT),
-	Column("PCIe x4 Slots", TEXT),
-	Column("PCIe x1 Slots", TEXT),
-	Column("M.2 Slots", TEXT),
-	Column("SATA 6.0 Gb/s", TEXT),
-	Column("Onboard Ethernet", TEXT),
-	Column("USB 2.0 Headers", TEXT),
-	Column("USB 3.2 Gen 1 Headers", TEXT),
-	Column("USB 3.2 Gen 2 Headers", TEXT),
-	Column("USB 3.2 Gen 2x2 Headers", TEXT),
-	Column("Wireless Networking", TEXT),
-	Column("RAID Support", TEXT),
-	Column("Price", TEXT),
-	Column("Url", TEXT)
-)
-memory = Table("memory", metadata,
-	Column("id", INTEGER, primary_key=True, autoincrement=True),
-	Column("Name", TEXT),
-	Column("Manufacturer", TEXT),
-	Column("Form Factor", TEXT),
-	Column("Modules", TEXT),
-	Column("Color", TEXT),
-	Column("CAS Latency", TEXT),
-	Column("Voltage", TEXT),
-	Column("ECC / Registered", TEXT),
-	Column("Heat Spreader", TEXT),
-	Column("Price", TEXT),
-	Column("Url", TEXT)
-)
-storage = Table("storage", metadata,
-	Column("id", INTEGER, primary_key=True, autoincrement=True),
-	Column("Name", TEXT),
-	Column("Manufacturer", TEXT),
-	Column("Capacity", TEXT),
-	Column("Type", TEXT),
-	Column("Cache", TEXT),
-	Column("Form Factor", TEXT),
-	Column("Interface", TEXT),
-	Column("NVME", TEXT),
-	Column("Price", TEXT),
-	Column("Url", TEXT)
-)
-psu = Table("psu", metadata,
-	Column("id", INTEGER, primary_key=True, autoincrement=True),
-	Column("Name", TEXT),
-	Column("Manufacturer", TEXT),
-	Column("Form Factor", TEXT),
-	Column("Efficiency Rating", TEXT),
-	Column("Wattage", TEXT),
-	Column("Length", TEXT),
-	Column("Modular", TEXT),
-	Column("Color", TEXT),
-	Column("Type", TEXT),
-	Column("Fanless", TEXT),
-	Column("ATX Connectors", TEXT),
-	Column("EPS Connectors", TEXT),
-	Column("PCIe 12-Pin Connectors", TEXT),
-	Column("PCIe 8-Pin Connectors", TEXT),
-	Column("PCIe 6+2-Pin Connectors", TEXT),
-	Column("PCIe 6-Pin Connectors", TEXT),
-	Column("SATA Connectors", TEXT),
-	Column("Molex 4-Pin Connectors", TEXT),
-	Column("Price", TEXT),
-	Column("Url", TEXT)
-)
-case = Table("case", metadata,
-	Column("id", INTEGER, primary_key=True, autoincrement=True),
-	Column("Name", TEXT),
-	Column("Manufacturer", TEXT),
-	Column("Type", TEXT),
-	Column("Color", TEXT),
-	Column("Power Supply", TEXT),
-	Column("Side Panel Window", TEXT),
-	Column("Power Supply Shroud", TEXT),
-	Column("Motherboard Form Factor", TEXT),
-	Column("Maximum Video Card Length", TEXT),
-	Column("Volume", TEXT),
-	Column("Dimensions", TEXT),
-	Column("Price", TEXT),
-	Column("Url", TEXT)
-)
 metadata.create_all(engine)
 
 
 
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-
 Base = declarative_base()
 
 class CPU(Base):
-	__tablename__ = 'cpu'
-	id = Column(Integer, primary_key=True, autoincrement=True)
-	Name = Column(String)
-	Manufacturer = Column(String)
-	Core_Count = Column(String)
-	Performance_Core_Clock = Column(String)
-	Performance_Boost_Clock = Column(String)
-	TDP = Column(String)
-	Integrated_Graphics = Column(String)
-	L3_Cache = Column(String)
-	Simultaneous_Multithreading = Column(String)
-	Includes_CPU_Cooler = Column(String)
-	Socket = Column(String)
-	Price = Column(String)
-	Url = Column(String)
+	__tablename__ = 'cpu',
 
-	def __repr__(self):
-		return f"<CPU(id={self.id}, Name={self.Name}, Manufacturer={self.Manufacturer}, Price={self.Price})>"
+	Core_Count = Column("Core Count", text),
+	Thread_Count = Column("Thread Count", text),
+	Performance_Base_Clock = Column("Base Clock", text),
+	Performance_Boost_Clock = Column("Boost Clock", text),
+	L3_Cache = Column("L3 Cache", text),
+	Socket = Column("Socket", text),
+	PCIe_Ver = Column("PCie Version", text),
+	Includes_CPU_Cooler = Column("Cpu Cooler", text),
+	TDP = Column("TDP", text),
+	Integrated_Graphics = Column("Integrated GPU", text)
+
+Base.metadata.create_all(engine)
