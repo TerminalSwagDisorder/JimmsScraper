dce2edb535b7aa5097895e8f833a9ffee567d145
TerminalSwagDisorder <44695050+TerminalSwagDisorder@users.noreply.github.com>
2023-04-04 03:04:41 +0300

    Cleaned layout to be more readable

--- Curr_dev/database.py
+++ Curr_dev/database.py
@@ -11,131 +11,128 @@ from sqlalchemy.sql.expression import ColumnClause
 from sqlalchemy.sql import table, column, select, update, insert, delete, text
 from sqlalchemy.ext.declarative import declarative_base
 
-fPath = os.path.abspath(os.path.realpath(__file__))
-dPath = os.path.dirname(fPath)
-finPath = dPath + "\\database"
 
-# Create database folder if it does not exist
-if not os.path.exists(finPath):
-	os.makedirs(finPath)
+def create_database():
+	# Create database folder if it does not exist
+	fPath = os.path.abspath(os.path.realpath(__file__))
+	dPath = os.path.dirname(fPath)
+	finPath = dPath + "\\database"
 
-engine = create_engine("sqlite:///" + finPath + "\\pcbuildwebsite_db.db", echo=True, pool_pre_ping=True)
-Session = sessionmaker(bind=engine)
-session = Session()
+	if not os.path.exists(finPath):
+		os.makedirs(finPath)
 
-# Define metadata information
-metadata = MetaData()
+	engine = create_engine("sqlite:///" + finPath + "\\pcbuildwebsite_db.db", echo = True, pool_pre_ping = True)
+	Session = sessionmaker(bind = engine)
+	session = Session()
 
-main_parts = ["cpu", "gpu", "cooler", "motherboard", "memory", "storage", "psu", "case"]
+	# Define metadata information
+	metadata = MetaData()
+
+	return engine, session, metadata
+
+engine, session, metadata = create_database()
 
 
 Base = declarative_base()
 # Create table in database	 
 class UniversalComponents(Base):
 	__abstract__ = True
-	ID = Column(INTEGER, primary_key=True, autoincrement=True)
-	Name = Column(TEXT)
-	Manufacturer = Column(TEXT)
-	Price = Column(TEXT)
-	Url = Column(TEXT)
+	ID = Column("ID", INTEGER, primary_key = True, autoincrement = True)
+	Name = Column("Name", TEXT)
+	Manufacturer = Column("Manufacturer", TEXT)
+	Price = Column("Price", TEXT)
+	Url = Column("Url", TEXT)
 
-	__tablename__ = 'universal_components'
+	__tablename__ = "universal_components"
 	__mapper_args__ = {
-		'polymorphic_identity': 'universal_components',
-		'concrete': True
+		"polymorphic_identity": "universal_components",
+		"concrete": True
 	}
 
 class CPU(UniversalComponents):
-	__tablename__ = 'cpu'
-
-	Core_Count = Column("Core Count", TEXT)
-	Thread_Count = Column("Thread Count", TEXT)
-	Performance_Base_Clock = Column("Base Clock", TEXT)
-	Performance_Boost_Clock = Column("Boost Clock", TEXT)
-	L3_Cache = Column("L3 Cache", TEXT)
-	Socket = Column("Socket", TEXT)
-	PCIe_Ver = Column("PCie Version", TEXT)
-	Includes_CPU_Cooler = Column("Cpu Cooler", TEXT)
-	TDP = Column("TDP", TEXT)
-	Integrated_Graphics = Column("Integrated GPU", TEXT)
+	__tablename__ = "cpu"
+	__table_args__ = (
+		Column("Core Count", TEXT),
+		Column("Thread Count", TEXT),
+		Column("Base Clock", TEXT),
+		Column("Boost Clock", TEXT),
+		Column("L3 Cache", TEXT),
+		Column("Socket", TEXT),
+		Column("PCie Version", TEXT),
+		Column("Cpu Cooler", TEXT),
+		Column("TDP", TEXT),
+		Column("Integrated GPU", TEXT)
+	)
 
 class GPU(UniversalComponents):
-	__tablename__ = 'gpu'
+	__tablename__ = "gpu"
 
-	Color = Column("Color", TEXT)
-	Memory = Column("Memory", TEXT)
-	Memory_Typ = Column("Memory Type", TEXT)
-	Core_Clock = Column("Core Clock", TEXT)
-	Boost_Clock = Column("Boost Clock", TEXT)
-	Effective = Column("Effective Memory Clock", TEXT)
-	Interface = Column("Interface", TEXT)
-	TDP = Column("TDP", TEXT)
-	Cooling = Column("Cooling", TEXT)
+	__table_args__ = (
+		Column("Color", TEXT),
+		Column("Memory", TEXT),
+		Column("Memory Type", TEXT),
+		Column("Core Clock", TEXT),
+		Column("Boost Clock", TEXT),
+		Column("Effective Memory Clock", TEXT),
+		Column("Interface", TEXT),
+		Column("TDP", TEXT),
+		Column("Cooling", TEXT)
+	)
 
 class Cooler(UniversalComponents):
-	__tablename__ = 'cpu cooler'
-	
-	Color = Column("Color", TEXT)
-	Fan_RPM = Column("Fan RPM", TEXT)
-	Noise_Level = Column("Noise Level", TEXT)
-	Height = Column("Height", TEXT)
+	__tablename__ = "cpu cooler"
+
+	__table_args__ = (
+		Column("Color", TEXT),
+		Column("Fan RPM", TEXT),
+		Column("Noise Level", TEXT),
+		Column("Height", TEXT)
+	)
 
 class Motherboard(UniversalComponents):
-	__tablename__ = 'motherboard'
+	__tablename__ = "motherboard"
 
-	Color = Column("Color", TEXT)
-	Mermory_Type = Column("Mermory Type", TEXT)
-	Memory_Max = Column("Memory Max", TEXT)
-	Memory_Slots = Column("Memory Slots", TEXT)
+	__table_args__ = (
+		Column("Color", TEXT),
+		Column("Mermory Type", TEXT),
+		Column("Memory Max", TEXT),
+		Column("Memory Slots", TEXT)
+	)
 
 class Memory(UniversalComponents):
-	__tablename__ = 'memory'
+	__tablename__ = "memory"
 
-	Modules = Column("Modules", TEXT)
-	Color = Column("Color", TEXT)
+	__table_args__ = (
+		Column("Modules", TEXT),
+		Column("Color", TEXT)
+	)
 
 class Storage(UniversalComponents):
-	__tablename__ = 'storage'
+	__tablename__ = "storage"
 
-	Capacity = Column("Capacity", TEXT)
-	Type = Column("Type", TEXT)
-	Interface = Column("Interface", TEXT)
+	__table_args__ = (
+		Column("Capacity", TEXT),
+		Column("Type", TEXT),
+		Column("Interface", TEXT)
+	)
 
 class PSU(UniversalComponents):
-	__tablename__ = 'psu'
+	__tablename__ = "psu"
 
-	is_ATX12V = Column("is_ATX12V", TEXT)
-	Dimensions = Column("Dimensions", TEXT)
+	__table_args__ = (
+		Column("is_ATX12V", TEXT),
+		Column("Dimensions", TEXT)
+	)
 
 class Case(UniversalComponents):
-	__tablename__ = 'case'
+	__tablename__ = "case"
 
-	Color = Column("Color", TEXT)
-	Size = Column("Size", TEXT)
-	Materials = Column("Materials", TEXT)
-	Compatibility = Column("Compatibility", TEXT)
+	__table_args__ = (
+		Column("Color", TEXT),
+		Column("Size", TEXT),
+		Column("Materials", TEXT),
+		Column("Compatibility", TEXT)
+
+	)
 
 Base.metadata.create_all(engine)
-
-
-# For specifying a order use __table_args__ 
-'''
-class CPU(UniversalComponents):
-    __tablename__ = 'cpu'
-    __table_args__ = (
-        Column('ID', INTEGER, primary_key=True, autoincrement=True),
-        Column('Name', TEXT),
-        Column('Core Count', TEXT),
-        Column('Thread Count', TEXT),
-        Column('Base Clock', TEXT),
-        Column('Boost Clock', TEXT),
-        Column('L3 Cache', TEXT),
-        Column('Socket', TEXT),
-        Column('PCie Version', TEXT),
-        Column('Cpu Cooler', TEXT),
-        Column('TDP', TEXT),
-        Column('Integrated GPU', TEXT),
-        Column('Price', TEXT),
-        Column('Url', TEXT)
-    )
-'''
