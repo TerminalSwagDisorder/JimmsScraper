0d2033ff6c3feaf98517518b4083277f29ed2bbd
TerminalSwagDisorder <44695050+TerminalSwagDisorder@users.noreply.github.com>
2023-04-14 01:45:24 +0300

    Added error checking

--- Curr_dev/dev_scraper.py
+++ Curr_dev/dev_scraper.py
@@ -24,8 +24,8 @@ def main():
 
 	driver = webdriver.Chrome(executable_path=driver_path)
 	
-	index_pages, index_pages_dict = get_subpages(base_URL, component_URL, driver)
-	all_product_links = get_urls(base_URL, index_pages, index_pages_dict)
+	index_pages_dict = get_subpages(base_URL, component_URL, driver)
+	all_product_links = get_urls(base_URL, index_pages_dict)
 	data_scraper(base_URL, all_product_links)
 
 def check_record_exists(session, main_parts, name):
@@ -73,75 +73,98 @@ def check_record_exists(session, main_parts, name):
 			
 def get_subpages(base_URL, component_URL, driver):
 	index_pages_dict = {}
-	index_pages = []
 	
 	#Get subpages for all components
-	for item in component_URL:		
-		parameter = "?p="
-		
-		page_index = [1]
+	try: 
+		for item in component_URL:
+			parameter = "?p="
 
-		print("Getting all pages for", item)
-		
-		# Go to page using Selenium
-		driver.get(base_URL + item + "?p=1")
-		wait = WebDriverWait(driver, 10)
-		button = wait.until(EC.element_to_be_clickable((By.XPATH, "//a[@data-bind='click: moveToLastPage']")))
-		driver.execute_script("arguments[0].scrollIntoView();", button)
-		driver.execute_script("arguments[0].click();", button)
-		sleep(2)
-		
-		# Get the URL
-		last_page_url = driver.current_url
-		last_page_number = int(last_page_url.split("=")[-1])
-		page_index.append(last_page_number)
-		
-		# Add subpage numbers to page_index
-		page_nums = range(page_index[0] + 1, page_index[1])
-		for num in page_nums:
-			page_index.append(num)
-			page_index.sort()
-		
-		# Create a new list with added parameter
-		pages_with_param = [parameter + str(page) for page in page_index]
+			page_index = [1]
 
-		# Create a list/dictionary with all subpages
-		index_pages_dict[item] = [parameter + str(page) for page in page_index]
-		index_pages.append(pages_with_param)
-		
-		sleep(2)
+			print("Getting all pages for", item)
 
-		
+			# Go to page using Selenium
+			driver.get(base_URL + item + "?p=1")
+			wait = WebDriverWait(driver, 10)
+			button = wait.until(EC.element_to_be_clickable((By.XPATH, "//a[@data-bind='click: moveToLastPage']")))
+			driver.execute_script("arguments[0].scrollIntoView();", button)
+			driver.execute_script("arguments[0].click();", button)
+			sleep(2)
+
+			# Get the URL
+			last_page_url = driver.current_url
+			last_page_number = int(last_page_url.split("=")[-1])
+			page_index.append(last_page_number)
+
+			# Add subpage numbers to page_index
+			page_nums = range(page_index[0] + 1, page_index[1])
+			for num in page_nums:
+				page_index.append(num)
+				page_index.sort()
+
+			# Create a new list with added parameter
+			pages_with_param = [parameter + str(page) for page in page_index]
+
+			# Create a list/dictionary with all subpages
+			index_pages_dict[item] = [parameter + str(page) for page in page_index]
+
+			sleep(2)
+	except Exception as e:
+		print(f"Error while processing {item}: {e}")
+	else:
+		print("All subpages scraped")
+
+
 	driver.close()
 	driver.quit()
-
-	return index_pages, index_pages_dict
 	
-def get_urls(base_URL, index_pages, index_pages_dict):
+
+	return index_pages_dict
+	
+def get_urls(base_URL, index_pages_dict):
 	all_product_links = []
+	
+	# Get links for all the products
 	for key, value in index_pages_dict.items():
 		for index in value:
-			print(base_URL + key + index)
-			product_list_page = requests.get(base_URL + key + index)
-			next_soup = BeautifulSoup(product_list_page.content, "html.parser")
-			page_content = next_soup.find("div", class_="product-list-wrapper")
-			product_name = page_content.find_all("h5", class_="product-box-name")
-			for item in product_name:
-				product_link = item.find("a", href = True)
-				get_link = product_link.get("href")
+			curr_url = base_URL + key + index
+			print(curr_url)
+			
+			# Parse and iterate through the html using bs4
+			try: 
+				product_list_page = requests.get(curr_url)
+				next_soup = BeautifulSoup(product_list_page.content, "html.parser")
+				page_content = next_soup.find("div", class_="product-list-wrapper")
+				product_name = page_content.find_all("h5", class_="product-box-name")
+				
 
-				all_product_links.append(get_link)
-	
+				# Get the actual link for each item
+				for item in product_name:
+					if "Tarjous" not in item.text and "Bundle" not in item.text:
+						product_link = item.find("a", href = True)
+						get_link = product_link.get("href")
+
+						all_product_links.append(get_link)
+						sleep(0.1)
+					else:
+						print("Skipped reduced or bundled item")
+			except Exception as e:
+				print(f"Error while processing {curr_url}: {e}")
+			else:
+				print(f"{len(all_product_links)} links found")
+					
+
 	return all_product_links
 
 def data_scraper(base_URL, all_product_links):
-	for component in component_URL2:
-		item_Page = requests.get(component, allow_redirects=True)
-		item_Soup = BeautifulSoup(item_Page.content, "html.parser")
+	for product in all_product_links:
+		curr_link = base_URL + product
+		item_page = requests.get(curr_link, allow_redirects=True)
+		item_soup = BeautifulSoup(item_page.content, "html.parser")
 		
-		results_item = item_Soup.find("div", itemprop="description")
+		results_item = item_soup.find("div", itemprop="description")
 		desc_data = results_item.find("p")
 		print(desc_data)
-			
+		sleep(0.1)
 
 main()
