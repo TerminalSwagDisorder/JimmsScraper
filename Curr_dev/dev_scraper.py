737d0da51d7760cc24ca322b69260be7c73c0978
TerminalSwagDisorder <44695050+TerminalSwagDisorder@users.noreply.github.com>
2023-04-16 04:43:37 +0300

    continued on the scraping function

--- Curr_dev/dev_scraper.py
+++ Curr_dev/dev_scraper.py
@@ -10,14 +10,15 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from pprint import pprint as pprint
 
 
 def main():
 	# Urls for jimms
 	base_URL = "https://www.jimms.fi"
 	product_URL = "/fi/Product/Show/"
-	component_URL = ["/fi/Product/List/000-00K/komponentit--kiintolevyt-ssd-levyt"]
-	component_URL2 = ["https://www.jimms.fi/fi/Product/Show/166056/100-100000065box/amd-ryzen-5-5600x-am4-3-7-ghz-6-core-boxed"]
+	component_URL = ["/fi/Product/List/000-00P/komponentit--naytonohjaimet"]
+	component_URL2 = ["https://www.jimms.fi/fi/Product/Show/187728/tuf-rtx4070ti-o12g-gaming/asus-geforce-rtx-4070-ti-tuf-gaming-oc-edition-naytonohjain-12gb-gddr6x-asus-cashback-70"]
 	component_temp = ["/fi/Product/List/000-00K/komponentit--kiintolevyt-ssd-levyt", "/fi/Product/List/000-00H/komponentit--emolevyt", "/fi/Product/List/000-00J/komponentit--kotelot", "/fi/Product/List/000-00M/komponentit--lisakortit", "/fi/Product/List/000-00N/komponentit--muistit", "/fi/Product/List/000-00P/komponentit--naytonohjaimet", "/fi/Product/List/000-00R/komponentit--prosessorit", "/fi/Product/List/000-00U/komponentit--virtalahteet"]
 
 	driver_path = "./chromedriver_win32/chromedriver.exe"
@@ -87,7 +88,6 @@ def get_subpages(base_URL, component_URL, driver):
 			driver.get(base_URL + item + "?p=1")
 			wait = WebDriverWait(driver, 10)
 			button = wait.until(EC.element_to_be_clickable((By.XPATH, "//a[@data-bind='click: moveToLastPage']")))
-			driver.execute_script("arguments[0].scrollIntoView();", button)
 			driver.execute_script("arguments[0].click();", button)
 			sleep(2)
 
@@ -145,7 +145,8 @@ def get_urls(base_URL, index_pages_dict):
 						get_link = product_link.get("href")
 
 						all_product_links.append(get_link)
-						sleep(0.1)
+						#print(get_link)
+						sleep(0.2)
 					else:
 						print("Skipped reduced or bundled item")
 			except Exception as e:
@@ -162,9 +163,65 @@ def data_scraper(base_URL, all_product_links):
 		item_page = requests.get(curr_link, allow_redirects=True)
 		item_soup = BeautifulSoup(item_page.content, "html.parser")
 		
+		# Get product category
+		category_link = item_soup.find_all("a", class_="link-secondary")[2]
+		get_category = category_link.get("href")
+		
+		# Get the data and trim it
 		results_item = item_soup.find("div", itemprop="description")
-		desc_data = results_item.find("p")
-		print(desc_data)
-		sleep(0.1)
+		data_start = results_item.find_all(["p", "ul", "li"])
+		
+		print(curr_link)
+		desc_data = results_item.find("strong", text="Tekniset tiedot")
+		if desc_data is not None:
+			trimmed_data = desc_data.find_all_next(recursive = False)
+			list_list = []
+			for item in trimmed_data:
+				if item.name in ["div", "a"]:
+					print("break:", item.name)
+					break
+				elif item.name in ["li", "ul"]:
+					list_item = item.text.strip("\n")
+					if list_item and list_item not in list_list:
+						print(list_item)
+						list_list.append(list_item)
+					#print(list_list)
+					#print(item.text)
+						
+		#splitlines()	#Splits the string at line breaks and returns a list
+
+		if "/fi/Product/List/000-00K" in get_category:
+			print("Drive")
+		elif "/fi/Product/List/000-00H" in get_category:
+			print("Mobo")
+		elif "/fi/Product/List/000-00J" in get_category:
+			print("Case")
+		elif "/fi/Product/List/000-00M" in get_category:
+			print("Addin")
+		elif "/fi/Product/List/000-00N" in get_category:
+			print("RAM")
+		elif "/fi/Product/List/000-00P" in get_category:
+			gpu_dict = {
+			"Color": None,
+			"Memory": None,
+			"Memory Type": None,
+			"Core Clock": None,
+			"Boost Clock": None,
+			"Effective Memory Clock": None,
+			"Interface": None,
+			"TDP": None,
+			"Cooling": None,
+			}
+		elif "/fi/Product/List/000-00R" in get_category:
+			print("CPU")
+		elif "/fi/Product/List/000-00U" in get_category:
+			print("PSU")
+			
+		else:
+			print("Something went wrong. Category:", get_category)
+		
+		
+		sleep(0.2)
+		#print(desc_data)
 
 main()
