bba8b0952511c328a678105b19c72e8613f428e1
TerminalSwagDisorder <44695050+TerminalSwagDisorder@users.noreply.github.com>
2023-06-11 00:07:07 +0300

    Update setup.py

--- Curr_dev/setup.py
+++ Curr_dev/setup.py
@@ -1,9 +1,9 @@
 import subprocess
 
 def install_dependencies():
-    subprocess.call(['pip', 'install', '-r', 'requirements.txt'])
+    subprocess.call(["pip", "install", "-r", "requirements.txt"])
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print("Installing dependencies...")
     install_dependencies()
     print("Dependencies installed successfully.")
