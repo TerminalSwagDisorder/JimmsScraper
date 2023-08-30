e07a4e08f03886631b87ae2f2e7e5b534255aca2
TerminalSwagDisorder <44695050+TerminalSwagDisorder@users.noreply.github.com>
2023-08-30 23:28:05 +0300

    Removed duplicate files

--- setup.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-def install_dependencies():
-    subprocess.call(["pip", "install", "-r", "requirements.txt"])
-
-if __name__ == "__main__":
-    print("Installing dependencies...")
-    install_dependencies()
-    print("Dependencies installed successfully.")
